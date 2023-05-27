5a98e4572f9b1653bde28effba0f320855503e3d
Bernhard Scholz <bernhard@fantom.foundation>
2023-05-27 16:56:13 +0000

    Fix transaction renumbering

--- evmcore/state_processor.go
+++ evmcore/state_processor.go
@@ -18,6 +18,7 @@ package evmcore
 
 import (
 	"math/big"
+	"math"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/state"
@@ -45,6 +46,12 @@ func NewStateProcessor(config *params.ChainConfig, bc DummyChain) *StateProcesso
 	}
 }
 
+// global variable tracking number of transactions in a block
+var (
+	txCounter      int
+	oldBlockNumber uint64 = math.MaxUint64
+)
+
 // Process processes the state changes according to the Ethereum rules by running
 // the transaction messages using the statedb and applying any rewards to both
 // the processor (coinbase) and any included uncles.
@@ -62,6 +69,10 @@ func (p *StateProcessor) Process(block *EvmBlock, statedb *state.StateDB, cfg vm
 		totalFee = new(big.Int)
 	)
 	// Iterate over and process the individual transactions
+	if oldBlockNumber != block.NumberU64() {
+		txCounter = 0
+		oldBlockNumber = block.NumberU64()
+	}
 	for i, tx := range block.Transactions {
 		msg, _ := tx.AsMessage(types.MakeSigner(p.config, block.Header().Number))
 		statedb.Prepare(tx.Hash(), block.Hash, i)
@@ -83,8 +94,9 @@ func (p *StateProcessor) Process(block *EvmBlock, statedb *state.StateDB, cfg vm
 			substate.NewSubstateMessage(&msg),
 			substate.NewSubstateResult(receipt),
 		)
-		substate.PutSubstate(block.NumberU64(), i, recording)
+		substate.PutSubstate(block.NumberU64(), txCounter, recording)
 
+		txCounter++
 		totalFee.Add(totalFee, fee)
 		receipts = append(receipts, receipt)
 		allLogs = append(allLogs, receipt.Logs...)
